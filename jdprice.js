455f5fee6344c7cbb9e3f78e7c53566716bd2705
Menfo <94981604+specialmenfo@users.noreply.github.com>
2021-12-05 13:38:42 +0800

    Update jdprice.js

--- jdprice.js
+++ jdprice.js
@@ -2,7 +2,7 @@
 JD | TB Price comparison
 by Small
 date 2021-06-25
-Thanks @yichahucha
+Thanks @yichahucha@
 
 QX:
 ^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig) url script-response-body https://service.2ti.st/QuanX/Script/jd_tb_price/main.js
