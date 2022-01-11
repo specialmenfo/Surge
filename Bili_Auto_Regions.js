837c7705398bc2219642f4d97bb12458caf69856
Menfo <94981604+specialmenfo@users.noreply.github.com>
2022-01-11 17:12:56 +0800

    Update Bili_Auto_Regions.js

--- Bili_Auto_Regions.js
+++ Bili_Auto_Regions.js
@@ -3,9 +3,9 @@ let run = EnvInfo();
 
 async function SwitchRegion(play) {
 	const Group = $.read('BiliArea_Policy') || '🌍 BiliBili 換區'; //Your blibli policy group name.
-	const CN = $.read('BiliArea_CN') || '🎯 全球直連'; //Your China sub-policy name.
-	const TW = $.read('BiliArea_TW') || '🇹🇼 台灣節點'; //Your Taiwan sub-policy name.
-	const HK = $.read('BiliArea_HK') || '🇭🇰 香港節點'; //Your HongKong sub-policy name.
+	const CN = $.read('BiliArea_CN') || 'DIRECT'; //Your China sub-policy name.
+	const TW = $.read('BiliArea_TW') || '🇹🇼 台灣Website'; //Your Taiwan sub-policy name.
+	const HK = $.read('BiliArea_HK') || '🇭🇰 香港Website'; //Your HongKong sub-policy name.
 	const current = await $.getPolicy(Group);
 	const area = (() => {
 		if (/\u50c5[\u4e00-\u9fa5]+\u6e2f|%20%E6%B8%AF&/.test(play)) {
