a3d71bd7c272c76d22582348ded6ba1a224441b9
Menfo <94981604+specialmenfo@users.noreply.github.com>
2022-01-12 18:24:16 +0800

    Delete reload.js

--- reload.js
+++
@@ -1,9 +0,0 @@
-$httpAPI("POST", "/v1/profiles/reload", {}, data => {
-    $notification.post("配置重載","成功","")
-    $done({
-        title: "Profile Reload",
-        content: "配置檔案重載成功",
-        icon: "arrow.triangle.2.circlepath.doc.on.clipboard",
-        "icon-color": "#8B81C3",
-     })
-    });
