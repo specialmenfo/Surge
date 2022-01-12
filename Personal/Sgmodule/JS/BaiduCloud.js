67d966135f617e88c8023cae8ea12aa919bba861
Menfo <94981604+specialmenfo@users.noreply.github.com>
2022-01-12 20:09:49 +0800

    Update BaiduCloud.js

--- Personal/Sgmodule/JS/BaiduCloud.js
+++ Personal/Sgmodule/JS/BaiduCloud.js
@@ -5,7 +5,7 @@
 QuantumultX:
 
 [rewrite_local]
-https:\/\/pan\.baidu\.com\/rest\/\d\.\d\/membership\/user url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/BaiduCloud.js
+https:\/\/pan\.baidu\.com\/rest\/\d\.\d\/membership\/user url script-response-body https://raw.githubusercontent.com/specialmenfo/Surge/master/Personal/Sgmodule/JS/BaiduCloud.js
 
 [mitm]
 hostname = pan.baidu.com
@@ -14,7 +14,7 @@ hostname = pan.baidu.com
 Surge4 or Loon:
 
 [Script]
-http-response https:\/\/pan\.baidu\.com\/rest\/\d\.\d\/membership\/user requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/BaiduCloud.js
+http-response https:\/\/pan\.baidu\.com\/rest\/\d\.\d\/membership\/user requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/specialmenfo/Surge/master/Personal/Sgmodule/JS/BaiduCloud.js
 
 [MITM]
 hostname = pan.baidu.com
