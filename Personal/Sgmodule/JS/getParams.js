e61561264fc5fd12a123c3c19cc33beb25ce2153
Menfo <94981604+specialmenfo@users.noreply.github.com>
2022-01-17 04:41:34 +0800

    Create getParams.js

---
+++ Personal/Sgmodule/JS/getParams.js
@@ -0,0 +1,64 @@
+let params = getParams($argument)
+
+!(async () => {
+/* 時間獲取 */
+let traffic = (await httpAPI("/v1/traffic","GET"))
+let dateNow = new Date()
+let dateTime = Math.floor(traffic.startTime*1000)
+let startTime = timeTransform(dateNow,dateTime)
+let title = params.title
+
+
+if ($trigger == "button") await httpAPI("/v1/dns/flush");
+
+  $done({
+      title:title,
+      content:`已運行: ${startTime}`,
+		icon: params.icon,
+		"icon-color":params.color
+    });
+
+})();
+
+function timeTransform(dateNow,dateTime) {
+let dateDiff = dateNow - dateTime;
+let days = Math.floor(dateDiff / (24 * 3600 * 1000));//計算出相差天數
+let leave1=dateDiff%(24*3600*1000)    //計算天數後剩餘的毫秒數
+let hours=Math.floor(leave1/(3600*1000))//計算出小時數
+//計算相差分鐘數
+let leave2=leave1%(3600*1000)    //計算小時數後剩餘的毫秒數
+let minutes=Math.floor(leave2/(60*1000))//計算相差分鐘數
+//計算相差秒數
+let leave3=leave2%(60*1000)      //計算分鐘數後剩餘的毫秒數
+let seconds=Math.round(leave3/1000)
+
+if(days==0){
+
+	if(hours==0){
+	if(minutes==0)return(`${seconds}秒`);
+	return(`${minutes}分${seconds}秒`)
+	}
+	return(`${hours}時${minutes}分${seconds}秒`)
+	}else {
+	return(`${days}天${hours}時${minutes}分`)
+	}
+
+}
+
+
+function httpAPI(path = "", method = "POST", body = null) {
+    return new Promise((resolve) => {
+        $httpAPI(method, path, body, (result) => {
+            resolve(result);
+        });
+    });
+}
+
+function getParams(param) {
+  return Object.fromEntries(
+    $argument
+      .split("&")
+      .map((item) => item.split("="))
+      .map(([k, v]) => [k, decodeURIComponent(v)])
+  );
+}
