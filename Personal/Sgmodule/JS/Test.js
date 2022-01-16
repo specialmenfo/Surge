e828b1bc00f0fd1acfc1449438c06c85e65448c3
Menfo <94981604+specialmenfo@users.noreply.github.com>
2022-01-17 05:29:45 +0800

    Delete Test.js

--- Personal/Sgmodule/JS/Test.js
+++
@@ -1,5 +0,0 @@
-with(document){
-write('<script type="text/javascript" language="javascript" src="/disney_check.js"></script>');
-write('<script type="text/javascript" language="javascript" src="/NetflixCheck.js"></script>');
-write('<script type="text/javascript" language="javascript" src="/YouTubePremiumCheck.js"></script>');
-} 
