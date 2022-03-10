814d23bdf1726058ee8d2aeef523e708403918c4
Menfo <94981604+specialmenfo@users.noreply.github.com>
2022-03-10 14:37:09 +0800

    Update nxyd_caiyun_svip.js

--- Personal/Sgmodule/JS/nxyd_caiyun_svip.js
+++ Personal/Sgmodule/JS/nxyd_caiyun_svip.js
@@ -13,12 +13,12 @@ const RIGHTS_REGEX = /https?:\/\/biz\.caiyunapp\.com\/membership_rights/;
 const RESULT = {
   is_vip: true,
   vip_type: "s",
-  svip_expired_at: 22221882066669.99999452950954,
+  svip_expired_at: 9999999999.9999999999,
 }
 const RESULT_WT = {
   vip: {
     enable: true,
-    svip_expired_at: 22221882066669.99999452950954
+    svip_expired_at: 9999999999.9999999999
   }
 }
 const RIGHTS = { 
