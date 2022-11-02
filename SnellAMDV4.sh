794f51efe350749fbd9740309f11d22d3cef6a22
Menfo <94981604+specialmenfo@users.noreply.github.com>
2022-11-02 14:45:03 +0800

    Update SnellAMDV4.sh

--- SnellAMDV4.sh
+++ SnellAMDV4.sh
@@ -5,7 +5,7 @@ CONF="/etc/snell/snell-server.conf"
 SYSTEMD="/etc/systemd/system/snell.service"
 apt-get install unzip -y
 cd ~/
-wget --no-check-certificate -O snell.zip https://dl.nssurge.com/snell/snell-server-v4.0.0-linux-aarch64.zip
+wget --no-check-certificate -O snell.zip https://dl.nssurge.com/snell/snell-server-v4.0.0-linux-amd64.zip
 unzip -o snell.zip
  rm -f snell.zip
  chmod +x snell-server
