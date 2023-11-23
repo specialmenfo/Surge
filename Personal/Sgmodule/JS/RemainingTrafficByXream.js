c169a89518173bad6e034218d6a7d5ff0b8d1084
Menfo <94981604+specialmenfo@users.noreply.github.com>
2023-11-23 13:03:59 +0800

    Update RemainingTrafficByXream.js

--- Personal/Sgmodule/JS/RemainingTrafficByXream.js
+++ Personal/Sgmodule/JS/RemainingTrafficByXream.js
@@ -18,7 +18,7 @@ async function operator(proxies = [], targetPlatform, env) {
     port: 80,
     cipher: 'aes-128-gcm',
     password: 'password',
-    name: `🌏 用咗: ${currT.value} ${currT.unit} / ${totalT.value} ${totalT.unit} ${date}`,
+    name: `🌏 用咗: ${currT.value} ${currT.unit} / ${totalT.value} ${totalT.unit}`,
   })
   return proxies
 }
